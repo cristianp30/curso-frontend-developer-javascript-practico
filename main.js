5bf182d441b1ec5970e258d9b2038a0ba770130b
cristian parra <pacc2430@gmail.com>
2022-11-08 22:17:04 -0500

    se agrega el menu del carrito de compras al html con su css y se agrega el codigo js para aparecer o no el menu del carrito

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const emailmenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-emailmenu.addEventListener('click', toggleDesktopMenu);
+
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+emailmenu.addEventListener('click', toggleDesktopMenu);
+menuCarritoIcon-addEventListener('click', toggleCarritoMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
@@ -12,4 +16,8 @@ function toggleDesktopMenu(){
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoMenu(){
+    aside.classList.toggle('inactive');
 }
