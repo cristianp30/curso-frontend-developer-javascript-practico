68df41bc732fec1acb68aeece94263cb3126c0dd
cristian parra <pacc2430@gmail.com>
2022-11-15 21:28:49 -0500

    se da dinamismo a los menus

--- main.js
+++ main.js
@@ -8,16 +8,25 @@ const mobileMenu = document.querySelector('.mobile-menu');
 
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 emailmenu.addEventListener('click', toggleDesktopMenu);
-menuCarritoIcon-addEventListener('click', toggleCarritoMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoMenu);
 
 function toggleDesktopMenu(){
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+function toggleCarritoMenu(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    if (!isMobileMenuClosed ||!isDesktopMenuClosed ) {
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+    
 }
 
-function toggleCarritoMenu(){
-    aside.classList.toggle('inactive');
+function toggleMobileMenu(){
+    aside.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
 }
